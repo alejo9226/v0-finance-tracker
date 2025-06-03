8dec9c4dedb2ceee691146a7cad472114c7ebc2d
Alejandro Alfaro <alejo9226@gmail.com>
2025-06-02 22:11:22 -0500

    Refactor: current currency code type

--- domain/entities/Asset.ts
+++ domain/entities/Asset.ts
@@ -1,11 +1,15 @@
-export const CURRENCIES = [
+export type CurrencyCode = {
+  code: string
+  symbol: string
+  name: string
+}
+
+export const CURRENCIES: CurrencyCode[] = [
   { code: 'USD', symbol: '$', name: 'US Dollar' },
   { code: 'ARS', symbol: '$', name: 'Argentine Peso' },
   { code: 'BRL', symbol: 'R$', name: 'Brazilian Real' },
   { code: 'COP', symbol: '$', name: 'Colombian Peso' },
-] as const
-
-export type CurrencyCode = (typeof CURRENCIES)[number]['code']
+]
 
 export interface Asset {
   id: string
